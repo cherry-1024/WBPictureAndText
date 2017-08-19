300b7df8f1c530df4465e10e837a6ab7791253f0
cherry-1024 <serv@csdc.info>
2017-08-19 16:14:47 +0800

    Initial Commit

---
+++ Weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weibo
+//
+//  Created by csdc on 2017/8/19.
+//  Copyright © 2017年 csdc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weibo
+//
+//  Created by csdc on 2017/8/19.
+//  Copyright © 2017年 csdc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
